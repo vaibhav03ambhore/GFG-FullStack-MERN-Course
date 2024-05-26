7df77c85a575368fa3cdc57919288524d3d5baf4
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-05-26 09:47:51 +0530

    GET http method to get userrecords

--- gfg-MERN-course/express-apps/server.js
+++ gfg-MERN-course/express-apps/server.js
@@ -2,6 +2,28 @@ const express= require('express');
 
 const app=express();
 
+let userData=[
+    {
+        username:'admin',
+        email:'admin@gmail.com'
+    },
+    {
+        username:'vaibhav03',
+        email:'vaibhav@gmail.com'
+    },
+    {
+        username:'manager',
+        email:'manager@gmail.com'
+    }
+];
+
+app.get('/loaduser',(req,res)=>{
+    res.send(userData);
+});
+
+
+
+
 app.use(express.static('public')); //serving the (by default) index.html file which is in the public folder
 
 app.listen(4000,()=>{
