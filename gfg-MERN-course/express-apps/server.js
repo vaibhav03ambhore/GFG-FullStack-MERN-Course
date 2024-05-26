0cd915dce67c207213d896cbcadd6e1507548487
vaibhav03ambhore <vaibhavambhore803@gmail.com>
2024-05-26 11:14:59 +0530

    implemented get,post,delete==> (adduser,finduser,getrecors,deleteuser)

--- gfg-MERN-course/express-apps/server.js
+++ gfg-MERN-course/express-apps/server.js
@@ -1,25 +1,90 @@
 const express= require('express');
+const bp=require('body-parser');
+var _=require('underscore');
 
 const app=express();
+app.use(bp.json());
+
+var uid=0;
+
 
 let userData=[
-    {
-        username:'admin',
-        email:'admin@gmail.com'
-    },
-    {
-        username:'vaibhav03',
-        email:'vaibhav@gmail.com'
-    },
-    {
-        username:'manager',
-        email:'manager@gmail.com'
-    }
+    
 ];
 
 app.get('/loaduser',(req,res)=>{
     res.send(userData);
 });
+
+app.get('/finduser/:id',(req,res)=>{
+    var uid=parseInt(req.params.id);
+    // console.log(uid);
+    
+    var foundData=_.findWhere(userData,{id:uid});
+    if(foundData) res.send(foundData);
+    else res.send('No user found');
+
+})
+
+app.delete('/deleteuser/:id',(req,res)=>{
+    var uid=parseInt(req.params.id);
+    // console.log(uid);
+    
+    var foundData=_.findWhere(userData,{id:uid});
+    if(foundData){
+        userData=_.without(userData,foundData);
+        res.send('User deleted successfully');
+    } 
+    else res.send('No user found');
+
+})
+
+
+app.post('/adduser',(req,res)=>{
+    var {username,email}=req.body;
+    var udata={id:++uid,username,email};
+    userData.push(udata);
+    res.send(`user added successfully`);
+})
+
+
+
+
+
+
+
+
+///projects task given by sir.
+
+
+let pid=0;
+
+let projectData=[
+    {
+        id:++pid,
+        name:'Project 1',
+        location:'Mumbai'
+    },
+    {
+        id:++pid,
+        name:'Project 2',
+        location:'Pune'
+    }
+    
+];
+
+app.get('/loadprojects',(req,res)=>{
+    res.send(projectData);
+});
+
+
+
+app.post('/addproject',(req,res)=>{
+    var {name,location}=req.body;
+    var pdata={id:++pid,name,location};
+    projectData.push(pdata);
+    res.send(`project added successfully`);
+})
 
 
 
